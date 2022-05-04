c7ef0f7628cec255d35b61a3a3aaf6161f95c09e
LuongMinhTien <binpro113wer@gmail.com>
2022-05-05 00:09:47 +0700

    remake PrefHelper

--- app/src/main/java/com/example/quanlydoan/data/PrefsHelper.java
+++ app/src/main/java/com/example/quanlydoan/data/PrefsHelper.java
@@ -9,92 +9,85 @@ import com.google.gson.Gson;
 
 public class PrefsHelper {
 
-    private static final String PREF_KEY_CURRENT_USER = "PREF_KEY_CURRENT_USER";
-    private static final String PREF_KEY_CURRENT_CART = "PREF_KEY_CURRENT_CART";
-    private static PrefsHelper instance;
-
+    //Shared Preferences lưu trữ thông tin dưới dạng key-value
     SharedPreferences sharedPreferences;
 
+    //khai báo key để lưu user va cart
+    private static final String PREF_KEY_CURRENT_USER = "PREF_KEY_CURRENT_USER";
+    private static final String PREF_KEY_CURRENT_CART = "PREF_KEY_CURRENT_CART";
+
+
+    //singleton class
+    //class PrefHelper sẽ khởi tạo 1 lần duy nhất và được lưu lại trong biến instace để dùng trong
+    // toàn bộ quá trình hoạt động của app
+    private static PrefsHelper instance;
+
+    //trả về instace của class PrefHelper
     public static PrefsHelper getInstance(Context context) {
-        if (instance == null) {
+        if (instance == null) { //PrefHelper chưa được khởi tạo
             instance = new PrefsHelper(context);
         }
         return instance;
     }
 
+
+    //SharedPrefernces cần tham số context để khởi tạo
+    /*
+    Context chứa thông tin toàn cục về môi trường ứng dụng.
+    Nó cho phép truy cập đến các tài nguyên ứng dụng (activity, fragment, intent,...)
+     */
     public PrefsHelper(Context context) {
+        //Khỏi tạo 1 SharedPreferences có tên là SharePrefs và đặt quyền riêng tư cho tập tin này là private
+        //xem dữ liệu ở View -> Tool Windows -> Device file explorer -> data -> data -> com.example.quanlydoan -> share_prefs
         this.sharedPreferences = context.getSharedPreferences("SharePrefs", Context.MODE_PRIVATE);
     }
 
-    @SuppressWarnings("unchecked")
-    public <T> T get(String key, T defaultValue) {
-        if (defaultValue instanceof String) {
-            return (T) sharedPreferences.getString(key, (String) defaultValue);
-        } else if (defaultValue instanceof Boolean) {
-            return (T) Boolean.valueOf(sharedPreferences.getBoolean(key, (Boolean) defaultValue));
-        } else if (defaultValue instanceof Float) {
-            return (T) Float.valueOf(sharedPreferences.getFloat(key, (Float) defaultValue));
-        } else if (defaultValue instanceof Integer) {
-            return (T) Integer.valueOf(sharedPreferences.getInt(key, (Integer) defaultValue));
-        } else if (defaultValue instanceof Long) {
-            return (T) Long.valueOf(sharedPreferences.getLong(key, (Long) defaultValue));
-        }
-        return null;
-    }
 
-    public <T> T get(String key, Class<T> tClass, T defaultValue) {
-        return (T) new Gson().fromJson(sharedPreferences.getString(key, (String) defaultValue), tClass);
-    }
 
-    public <T> void put(String key, T data) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        if (data instanceof String) {
-            editor.putString(key, (String) data);
-        } else if (data instanceof Boolean) {
-            editor.putBoolean(key, (Boolean) data);
-        } else if (data instanceof Float) {
-            editor.putFloat(key, (Float) data);
-        } else if (data instanceof Integer) {
-            editor.putInt(key, (Integer) data);
-        } else if (data instanceof Long) {
-            editor.putLong(key, (Long) data);
-        } else {
-            editor.putString(key, new Gson().toJson(data));
-        }
-        editor.apply();
-    }
-
-    public void clear() {
-        sharedPreferences.edit().clear().apply();
-    }
-
-    public void remove(String[] item) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        for (String s : item) {
-            editor.remove(s);
-        }
-        editor.apply();
-    }
-
-    public User getCurrentUser() {
-        return get(PREF_KEY_CURRENT_USER, User.class, null);
-    }
-
+    //thêm user vào SharedPreferences
     public void setCurrentUser(User user) {
-        put(PREF_KEY_CURRENT_USER, user);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        //thêm thông tin với key: PREF_KEY_CURRENT_USER và value là 1 json object
+        /* new Gson().toJson(object):
+        sử dụng thư viện gson được import trong file Grandle Scripts -> build.grandle (Module: QuanLyDoAn.app)
+        .....
+        dependencies {
+            ...
+            implementation 'com.google.code.gson:gson:2.9.0'
+            ...
+        }
+        .....
+
+        hàm toJson(Object object) dùng để convert Object thành kiểu json để lưu vào SharedPreference vì SharedPreference ko hỗ trợ lưu object
+         */
+        editor.putString(PREF_KEY_CURRENT_USER, new Gson().toJson(user));
+        editor.apply();
     }
 
+    //lấy dữ liệu user
+    public User getCurrentUser() {
+        // hàm fromJson(String json, Object object) để convert chuỗi json thành Object (ở dưới là object User)
+        return new Gson().fromJson(sharedPreferences.getString(PREF_KEY_CURRENT_USER, (String) ""), User.class);
+    }
+
+    //xóa value user khỏi SharedPreferences thông qua key PREF_KEY_CURRENT_USER
     public void removeCurrentUser() {
         sharedPreferences.edit().remove(PREF_KEY_CURRENT_USER).apply();
         removeCurrentCart();
     }
 
+
+
+
+
     public Order getCurrentCart() {
-        return get(PREF_KEY_CURRENT_CART, Order.class, null);
+        return new Gson().fromJson(sharedPreferences.getString(PREF_KEY_CURRENT_CART, (String) ""), Order.class);
     }
 
     public void setCurrentCart(Order order) {
-        put(PREF_KEY_CURRENT_CART, order);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(PREF_KEY_CURRENT_CART, new Gson().toJson(order));
+        editor.apply();
     }
 
     public void removeCurrentCart() {
